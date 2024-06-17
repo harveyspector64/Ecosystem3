f693d3efcfd502dd9c7fb2bc6528919fbe158597
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-16 18:52:11 -0700

    Delete game_logic.js

--- game_logic.js
+++
@@ -1,9 +0,0 @@
-function handleGameLogic(emojiType, x, y) {
-    if (emojiType === 'tree') {
-        unlockBird();
-    } else if (emojiType === 'bird') {
-        unlockWorm();
-    }
-
-    // Add more game logic as needed
-}
