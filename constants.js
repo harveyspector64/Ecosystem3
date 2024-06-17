5ba1310a7d0006630be28e6ed5332e321923b1e7
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-16 17:53:30 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,8 @@
+const EMOJIS = {
+    butterfly: '🦋',
+    tree: '🌳',
+    bird: '🐦',
+    caterpillar: '🐛' // Changed from worm to caterpillar emoji
+};
+
+const INITIAL_EMOJIS = ['butterfly', 'tree'];
