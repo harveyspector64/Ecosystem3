683f51b6a9ed5240a383a353596ff6df3961586f
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-20 20:12:03 -0700

    Update worm.js

--- worm.js
+++ worm.js
@@ -1,37 +1,62 @@
-function addWorm(x, y) {
-    const playArea = document.getElementById('play-area');
-    const wormElement = document.createElement('div');
-    wormElement.textContent = EMOJIS.WORM;
-    wormElement.classList.add('emoji', 'worm');
-    wormElement.style.position = 'absolute';
-    wormElement.style.left = `${x}px`;
-    wormElement.style.top = `${y}px`;
-    playArea.appendChild(wormElement);
+// Worm.js
 
-    // Start worm wiggling
-    startWormWiggle(wormElement);
+import { EMOJIS } from './constants.js';
+
+export class Worm {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.element = this.createWormElement();
+        this.wigglePhase = 0;
+        this.wiggleAmplitude = 2;
+        this.wiggleSpeed = 5;
+        this.visibilityTimer = 0;
+        this.isVisible = true;
+        this.lastRepositionTime = 0;
+    }
+
+    createWormElement() {
+        const element = document.createElement('div');
+        element.textContent = EMOJIS.WORM;
+        element.classList.add('emoji', 'worm');
+        element.style.position = 'absolute';
+        element.style.left = `${this.x}px`;
+        element.style.top = `${this.y}px`;
+        return element;
+    }
+
+    update(deltaTime) {
+        this.wiggle(deltaTime);
+        this.updateVisibility(deltaTime);
+        this.occasionallyReposition(deltaTime);
+    }
+
+    wiggle(deltaTime) {
+        this.wigglePhase += this.wiggleSpeed * deltaTime;
+        const wiggleOffset = Math.sin(this.wigglePhase) * this.wiggleAmplitude;
+        this.element.style.transform = `translateX(${wiggleOffset}px)`;
+    }
+
+    updateVisibility(deltaTime) {
+        this.visibilityTimer += deltaTime;
+        if (this.visibilityTimer > 10) {  // Every 10 seconds
+            this.visibilityTimer = 0;
+            this.isVisible = Math.random() < 0.8;  // 80% chance to be visible
+            this.element.style.opacity = this.isVisible ? '1' : '0';
+        }
+    }
+
+    occasionallyReposition(deltaTime) {
+        this.lastRepositionTime += deltaTime;
+        if (this.lastRepositionTime > 30) {  // Every 30 seconds
+            this.lastRepositionTime = 0;
+            if (Math.random() < 0.3) {  // 30% chance to reposition
+                const offset = 20;  // 20px offset
+                this.x += Math.random() * offset * 2 - offset;
+                this.y += Math.random() * offset * 2 - offset;
+                this.element.style.left = `${this.x}px`;
+                this.element.style.top = `${this.y}px`;
+            }
+        }
+    }
 }
-
-function startWormWiggle(worm) {
-    const wiggle = () => {
-        const wiggleAmount = Math.random() * 5; // Wiggle by up to 5px
-        const wiggleDirection = Math.random() > 0.5 ? 1 : -1; // Random direction
-
-        worm.style.transform = `translateX(${wiggleDirection * wiggleAmount}px)`;
-
-        // Randomize next wiggle time between 2 and 5 seconds
-        const wiggleTime = Math.random() * 3000 + 2000;
-
-        setTimeout(() => {
-            // Reset transformation after wiggle
-            worm.style.transform = '';
-            setTimeout(wiggle, wiggleTime);
-        }, 500); // Wiggle duration
-    };
-
-    // Initial delay before the first wiggle
-    setTimeout(wiggle, Math.random() * 3000 + 2000);
-}
-
-// Make the startWormWiggle function globally accessible
-window.startWormWiggle = startWormWiggle;
