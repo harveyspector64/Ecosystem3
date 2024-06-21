ef9c27452505367bedb54ebc2ba276151e3a660d
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-20 19:58:52 -0700

    Update ecosystemManager.js

--- ecosystemManager.js
+++ ecosystemManager.js
@@ -1,114 +1,141 @@
 // ecosystemManager.js
 
-import { Bush } from './entities/Bush.js';
-import { Tree } from './entities/Tree.js';
-import { Bird } from './entities/Bird.js';
-import { Butterfly } from './entities/Butterfly.js';
-import { Worm } from './entities/Worm.js';
+import { Bird } from './Bird.js';
+import { Butterfly } from './Butterfly.js';
+import { Worm } from './Worm.js';
+import { EMOJIS } from './constants.js';
 
 export class EcosystemManager {
-    constructor() {
+    constructor(playArea) {
+        this.playArea = playArea;
         this.entities = {
-            bushes: [],
-            trees: [],
             birds: [],
             butterflies: [],
-            worms: []
+            worms: [],
+            bushes: [],
+            trees: []
         };
-        this.grid = {}; // Simple spatial partitioning
-        this.gridSize = 100; // Size of each grid cell
+        this.firstBirdLanded = false;
     }
 
     addBush(x, y) {
-        const bush = new Bush(x, y);
-        this.entities.bushes.push(bush);
-        this.addToGrid(bush);
-        this.spawnButterflies(bush);
+        const bushElement = document.createElement('div');
+        bushElement.textContent = EMOJIS.BUSH;
+        bushElement.classList.add('emoji', 'bush');
+        bushElement.style.position = 'absolute';
+        bushElement.style.left = `${x}px`;
+        bushElement.style.top = `${y}px`;
+        this.playArea.appendChild(bushElement);
+        this.entities.bushes.push(bushElement);
+        this.spawnButterflies(bushElement);
     }
 
     addTree(x, y) {
-        const tree = new Tree(x, y);
-        this.entities.trees.push(tree);
-        this.addToGrid(tree);
-        this.spawnBird(x, y);
+        const treeElement = document.createElement('div');
+        treeElement.textContent = EMOJIS.TREE;
+        treeElement.classList.add('emoji', 'tree');
+        treeElement.style.position = 'absolute';
+        treeElement.style.left = `${x}px`;
+        treeElement.style.top = `${y}px`;
+        this.playArea.appendChild(treeElement);
+        this.entities.trees.push(treeElement);
+        this.addBird(x, y);
     }
 
     addWorm(x, y) {
         const worm = new Worm(x, y);
+        this.playArea.appendChild(worm.element);
         this.entities.worms.push(worm);
-        this.addToGrid(worm);
+    }
+
+    addBird(x, y) {
+        const bird = new Bird(x, y);
+        this.playArea.appendChild(bird.element);
+        this.entities.birds.push(bird);
     }
 
     spawnButterflies(bush) {
         const count = Math.floor(Math.random() * 3) + 1; // 1-3 butterflies
         for (let i = 0; i < count; i++) {
-            const butterfly = new Butterfly(bush.x, bush.y, bush);
+            const butterfly = new Butterfly(bush);
+            this.playArea.appendChild(butterfly.element);
             this.entities.butterflies.push(butterfly);
-            this.addToGrid(butterfly);
         }
-    }
-
-    spawnBird(x, y) {
-        const bird = new Bird(x, y);
-        this.entities.birds.push(bird);
-        this.addToGrid(bird);
     }
 
     update(deltaTime) {
-        for (let type in this.entities) {
-            this.entities[type].forEach(entity => {
-                entity.update(deltaTime);
-                this.updateGridPosition(entity);
-            });
-        }
-
-        this.handleInteractions();
-    }
-
-    handleInteractions() {
         this.entities.birds.forEach(bird => {
-            if (bird.state === 'flying') {
-                this.checkBirdButterflyCatch(bird);
-            } else if (bird.state === 'walking') {
-                this.checkBirdWormCatch(bird);
-            }
+            bird.update(deltaTime, this.playArea);
+            this.checkBirdInteractions(bird);
         });
 
         this.entities.butterflies.forEach(butterfly => {
-            this.checkButterflyPollination(butterfly);
+            butterfly.update(deltaTime);
+        });
+
+        this.entities.worms.forEach(worm => {
+            worm.update(deltaTime);
         });
     }
 
+    checkBirdInteractions(bird) {
+        if (bird.currentState === Bird.birdStates.FLYING) {
+            this.checkBirdButterflyCatch(bird);
+        } else if (bird.currentState === Bird.birdStates.WALKING) {
+            this.checkBirdWormDetection(bird);
+        }
+
+        if (!this.firstBirdLanded && bird.currentState === Bird.birdStates.WALKING) {
+            this.firstBirdLanded = true;
+            window.addWormToPanel();
+        }
+    }
+
     checkBirdButterflyCatch(bird) {
-        const nearbyButterflies = this.getNearbyEntities(bird, 'butterflies');
-        for (let butterfly of nearbyButterflies) {
-            if (this.checkCollision(bird, butterfly)) {
+        this.entities.butterflies.forEach((butterfly, index) => {
+            if (this.checkCollision(bird.element, butterfly.element)) {
                 bird.eat(butterfly);
-                this.removeEntity(butterfly);
-                break;
+                butterfly.element.remove();
+                this.entities.butterflies.splice(index, 1);
             }
+        });
+    }
+
+    checkBirdWormDetection(bird) {
+        const nearestWorm = this.findNearestWorm(bird);
+        if (nearestWorm && this.getDistance(bird.element, nearestWorm.element) < 300) {
+            bird.birdMoveToWorm(nearestWorm, this.playArea);
         }
     }
 
-    checkBirdWormCatch(bird) {
-        const nearbyWorms = this.getNearbyEntities(bird, 'worms');
-        for (let worm of nearbyWorms) {
-            if (this.checkCollision(bird, worm)) {
-                bird.eat(worm);
-                this.removeEntity(worm);
-                break;
+    findNearestWorm(bird) {
+        let nearestWorm = null;
+        let minDistance = Infinity;
+        this.entities.worms.forEach(worm => {
+            const distance = this.getDistance(bird.element, worm.element);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestWorm = worm;
             }
-        }
+        });
+        return nearestWorm;
     }
 
-    checkButterflyPollination(butterfly) {
-        if (butterfly.hasLanded && Math.random() < 0.1) { // 10% chance of pollination
-            const nearbyBushes = this.getNearbyEntities(butterfly, 'bushes');
-            if (nearbyBushes.length > 0) {
-                const randomBush = nearbyBushes[Math.floor(Math.random() * nearbyBushes.length)];
-                randomBush.pollinate();
-            }
-        }
+    checkCollision(element1, element2) {
+        const rect1 = element1.getBoundingClientRect();
+        const rect2 = element2.getBoundingClientRect();
+        return !(rect1.right < rect2.left || 
+                 rect1.left > rect2.right || 
+                 rect1.bottom < rect2.top || 
+                 rect1.top > rect2.bottom);
+    }
+
+    getDistance(element1, element2) {
+        const rect1 = element1.getBoundingClientRect();
+        const rect2 = element2.getBoundingClientRect();
+        const dx = (rect1.left + rect1.right) / 2 - (rect2.left + rect2.right) / 2;
+        const dy = (rect1.top + rect1.bottom) / 2 - (rect2.top + rect2.bottom) / 2;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     removeEntity(entity) {
@@ -116,71 +143,7 @@ export class EcosystemManager {
         const index = this.entities[type].indexOf(entity);
         if (index > -1) {
             this.entities[type].splice(index, 1);
-            this.removeFromGrid(entity);
+            entity.element.remove();
         }
-    }
-
-    addToGrid(entity) {
-        const cellX = Math.floor(entity.x / this.gridSize);
-        const cellY = Math.floor(entity.y / this.gridSize);
-        const cellKey = `${cellX},${cellY}`;
-        if (!this.grid[cellKey]) {
-            this.grid[cellKey] = [];
-        }
-        this.grid[cellKey].push(entity);
-        entity.gridCell = cellKey;
-    }
-
-    updateGridPosition(entity) {
-        const newCellX = Math.floor(entity.x / this.gridSize);
-        const newCellY = Math.floor(entity.y / this.gridSize);
-        const newCellKey = `${newCellX},${newCellY}`;
-        if (newCellKey !== entity.gridCell) {
-            this.removeFromGrid(entity);
-            this.addToGrid(entity);
-        }
-    }
-
-    removeFromGrid(entity) {
-        if (entity.gridCell) {
-            const cellEntities = this.grid[entity.gridCell];
-            const index = cellEntities.indexOf(entity);
-            if (index > -1) {
-                cellEntities.splice(index, 1);
-            }
-        }
-    }
-
-    getNearbyEntities(entity, type) {
-        const cellX = Math.floor(entity.x / this.gridSize);
-        const cellY = Math.floor(entity.y / this.gridSize);
-        const nearbyEntities = [];
-        for (let dx = -1; dx <= 1; dx++) {
-            for (let dy = -1; dy <= 1; dy++) {
-                const cellKey = `${cellX + dx},${cellY + dy}`;
-                if (this.grid[cellKey]) {
-                    nearbyEntities.push(...this.grid[cellKey].filter(e => e.constructor.name.toLowerCase() + 's' === type));
-                }
-            }
-        }
-        return nearbyEntities;
-    }
-
-    checkCollision(entity1, entity2) {
-        const dx = entity1.x - entity2.x;
-        const dy = entity1.y - entity2.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-        return distance < (entity1.radius + entity2.radius);
-    }
-
-    getVisibleEntities(viewport) {
-        const visibleEntities = [];
-        for (let type in this.entities) {
-            visibleEntities.push(...this.entities[type].filter(entity => 
-                entity.x >= viewport.left && entity.x <= viewport.right &&
-                entity.y >= viewport.top && entity.y <= viewport.bottom
-            ));
-        }
-        return visibleEntities;
     }
 }
