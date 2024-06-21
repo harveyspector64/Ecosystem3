910800d7745b4f1d89646154793b7bb9c98713d6
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-21 16:57:16 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,12 +1,25 @@
-const EMOJIS = {
+// constants.js
+
+export const EMOJIS = {
     BUSH: '🌺',
     TREE: '🌳',
     BUTTERFLY: '🦋',
-    BIRD: '🐦',  // Red Robin emoji for general use
+    BIRD: '🐦',
     WORM: '🐛'
 };
 
-const INITIAL_EMOJIS = [
+export const INITIAL_EMOJIS = [
     { id: 'flowering-bush', emoji: EMOJIS.BUSH },
     { id: 'tree', emoji: EMOJIS.TREE, disabled: true }
 ];
+
+export const birdStates = {
+    PERCHING: 'perching',
+    FLYING: 'flying',
+    WALKING: 'walking',
+    MOVING_TO_WORM: 'movingToWorm',
+    EATING: 'eating',
+    DESCENDING: 'descending',
+    LANDING: 'landing',
+    ASCENDING: 'ascending'
+};
