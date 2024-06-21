cafb4bce5b2f610046a6fe957d65d8b67a400317
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-21 16:58:40 -0700

    Create gameState.js

---
+++ gameState.js
@@ -0,0 +1,41 @@
+// gameState.js
+
+export const gameState = {
+    selectedEmoji: null,
+    draggedElement: null,
+    firstBirdLanded: false,
+    bushesPlanted: 0,
+    treesPlanted: 0,
+    butterflies: []
+};
+
+export function setSelectedEmoji(emoji) {
+    gameState.selectedEmoji = emoji;
+}
+
+export function setDraggedElement(element) {
+    gameState.draggedElement = element;
+}
+
+export function setFirstBirdLanded() {
+    gameState.firstBirdLanded = true;
+}
+
+export function incrementBushesPlanted() {
+    gameState.bushesPlanted++;
+}
+
+export function incrementTreesPlanted() {
+    gameState.treesPlanted++;
+}
+
+export function addButterfly(butterfly) {
+    gameState.butterflies.push(butterfly);
+}
+
+export function removeButterfly(butterfly) {
+    const index = gameState.butterflies.indexOf(butterfly);
+    if (index > -1) {
+        gameState.butterflies.splice(index, 1);
+    }
+}
